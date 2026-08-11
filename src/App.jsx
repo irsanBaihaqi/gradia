9f33c2d90e65fd98f0218eb88fbd149100872adf
irsanBaihaqi <irsanhaqi5@gmail.com>
2026-08-11 11:26:59 +0700

    setup project and installing vite, tailwind, and gsap

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section>
+        <h1>Hello</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
